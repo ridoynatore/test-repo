b268a3bb7b38a98ca1accc1c458fda232e43e6fa
Ridoynatore <ridoyislamnatore17@gmail.com>
2024-03-22 16:36:33 +0600

    sd

---
+++ app.js
@@ -0,0 +1,13 @@
+// let duplicate = ["ridoy", "hasan", "ridoy", "ridoy", "abdul", "korim", "nafiz"];
+
+// let votarList = [];
+
+// for (let i = 0; i < duplicate.length; i++) {
+//     if (votarList.indexOf(duplicate[i])=== -1) {
+//         votarList.push(duplicate[i]); 
+//     }  
+// }
+
+// console.log(votarList);
+
+
